a0041f9347679a42fbb31b32ab1f33489c3c9e93
Monimoralesa <monimoralesa@gmail.com>
2022-11-06 17:40:57 -0500

    Modify S3

--- s3.tf
+++ s3.tf
@@ -1,14 +1,11 @@
 resource "aws_s3_bucket" "codepipeline_artifacts" {
   bucket = "pipeline-artifacts-jmonicam"
-  acl = "private"
 }
 
 resource "aws_s3_bucket" "codepipeline_artifacts1" {
   bucket = "pipeline-test-jmonicam1-cr3"
-  acl = "private"
 }
 
 resource "aws_s3_bucket" "codepipeline_artifacts2" {
   bucket = "pipeline-dev-jmonicam1-cr3"
-  acl = "private"
 }
